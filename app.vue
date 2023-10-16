477deafdad2febb1b9f3528da4936f64c78e7574
James <jamesga777@gmail.com>
2023-10-16 20:18:30 +0800

    Added PWA feature

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <VitePwaManifest />
     <NuxtWelcome />
   </div>
 </template>
+<script setup lang="ts">
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,44 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// https://logo.com/docs/api/configuration/logo-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@vite-pwa/nuxt',
+  ],
+  pwa: {
+    manifest: {
+      name: 'Unitap',
+      short_name: 'Unitap',
+      description: 'Testing PWA in unitap',
+      icons: [
+        {
+          src: 'logo.png',
+          sizes: '64x64',
+          type: 'image/png',
+        },
+        {
+          src: 'logo.png',
+          sizes: '144x144',
+          type: 'image/png',
+        },
+        {
+          src: 'logo.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'logo.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ]
+    },
+    workbox: {
+      navigateFallback: '/',
+    },
+    devOptions: {
+      enabled: true,
+      type: "module",
+    },
+
+  }
 })
