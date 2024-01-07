040d518e22493beafddb2ce03ba877a43fccc710
James <jamesga777@gmail.com>
2024-01-07 20:57:57 +0800

    Fixed student name

---
+++ components/HeatMap.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  asd <br>
+  asd <br>
+  asd <br>
+  asd <br>
+  asd <br>
+  asd <br>
+  asd <br>
+  asd <br>
+  <div  class="main relative" >
+      <UPopover :popper="{ placement: 'auto' }" class="HPSB absolute top-[168px] left-[270px]">
+        <UButton color="white" label="" class="p-3 rounded-full"/>
+        <template #panel>
+          <div class="p-4">
+            <Placeholder class="h-20 w-48" />
+            asdadasdsad
+          </div>
+        </template>
+      </UPopover>
+
+
+    <img src="/Campus-map.jpg"/>
+    <h3 class="font-light text-xs justify-center m-0 bg-gray-800 rounded-b-md"><a href="https://www.umak.edu.ph/about/campus-map/" target="_blank">Artwork by Prof. Stanley Rovira.</a></h3>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ pages/map.vue
@@ -0,0 +1,28 @@
+<template>
+  <HeatMap class="w-screen"/>
+</template>
+
+
+<style scoped>
+
+#double {
+
+  @apply flex justify-center h-full;
+
+  flex-direction: var(--article-split-dir);
+
+  > aside {
+    @apply flex flex-col h-full px-[var(--article-aside-p)];
+  }
+}
+
+aside > div {
+  @apply bg-[var(--l30)] rounded-xl p-4 min-w-[340px];
+
+  ::before {
+    @apply m-0
+  }
+}
+
+
+</style>
